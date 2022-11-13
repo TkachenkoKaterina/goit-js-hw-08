7fa6e19a8c36aa2114be6b4069ee0544d3c47555
Katerina <tkachenko.city@gmail.com>
2022-11-14 01:33:36 +0200

    Фуууууух)))

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+'use strict';
 
+console.log(5);
+
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = function (event) {
+  console.log(event);
+
+  const seconds = event.seconds;
+  const secondsToString = JSON.stringify(seconds);
+  localStorage.setItem('LOCALSTORAGE_KEY', secondsToString);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const getDataStorage = localStorage.getItem('LOCALSTORAGE_KEY');
+const DataStorageToNumber = JSON.parse(getDataStorage);
+console.log(DataStorageToNumber);
+
+player
+  .setCurrentTime(DataStorageToNumber)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+'use strict';
 
+var throttle = require('lodash.throttle');
+
+const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('.feedback-form input');
+const textareaRef = document.querySelector('.feedback-form textarea');
+const submitRef = document.querySelector('.feedback-form button');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+loadContentToForm();
+
+function onInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('LOCALSTORAGE_KEY', JSON.stringify(formData));
+}
+
+function loadContentToForm() {
+  const loadedContent = localStorage.getItem('LOCALSTORAGE_KEY');
+  const loadedContentForm = JSON.parse(loadedContent);
+  // console.log(loadedContentForm);
+
+  if (loadedContentForm) {
+    formData = loadedContentForm;
+    if (formData.email) {
+      inputRef.value = formData.email;
+    }
+    if (formData.message) {
+      textareaRef.value = formData.message;
+    }
+  }
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem('LOCALSTORAGE_KEY');
+  console.log(formData);
+}
+
+formRef.addEventListener('input', throttle(onInput, 500));
+formRef.addEventListener('submit', onSubmit);
